3bf6a023f30c007d0fd286097d22f25d083c344a
ZeevBinyaminov <zzbinyaminov@outlook.com>
2023-06-13 22:34:21 +0300

    first project is done

--- CyberSecurityApp/views.py
+++ CyberSecurityApp/views.py
@@ -7,20 +7,20 @@ def say_hello(request):
 
 
 def main_page(request):
-    return render(request, "CyberSecurityApp/main.html", {"title": "Main page"})
-
-
-def applications(request):
-    return render(request, "CyberSecurityApp/applications.html", {"title": "Applications"})
+    return render(request, "CyberSecurityApp/main.html", {"title": "Main page", "one": "disabled"})
 
 
 def tips_and_tricks(request):
-    return render(request, "CyberSecurityApp/tips_and_tricks.html", {"title": "Tips and tricks"})
+    return render(request, "CyberSecurityApp/tips_and_tricks.html", {"title": "Tips and tricks", "two": "disabled"})
 
 
 def useful_sources(request):
-    return render(request, "CyberSecurityApp/useful_sources.html", {"title": "Useful sources"})
+    return render(request, "CyberSecurityApp/useful_sources.html", {"title": "Useful sources", "three": "disabled"})
+
+
+def applications(request):
+    return render(request, "CyberSecurityApp/applications.html", {"title": "Applications", "four": "disabled"})
 
 
 def form(request):
-    return render(request, "CyberSecurityApp/forms.html", {"title": "Form"})
+    return render(request, "CyberSecurityApp/forms.html", {"title": "Form", "five": "disabled"})
